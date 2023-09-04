46adf4de3ee9e342192f487edbe9f4634364141f
ghill1 <ghill012@email.cpcc.edu>
2023-09-04 16:37:20 -0400

    Changed Output
    I added another line of output.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld
+{
    public static void main(String[] args)
    {
-      System.out.print("hello World!");
+      System.out.print("hello World!\n");
+      System.out.print("Program modified, but able to revert to original version" +
+                       " using GitHub desktop");
    } 
 }
